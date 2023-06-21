04bb929d7082f709fde9c23e9746bf798fe8ddd1
AhmedSaidTuncpinar <ahmtstncpnr@gmail.com>
2023-06-21 14:49:45 +0200

    update

---
+++ src/test/java/tests/elements/TextBox.java
@@ -0,0 +1,12 @@
+package tests.elements;
+
+import org.junit.Test;
+import utilities.TestBase;
+
+public class TextBox extends TestBase {
+
+    @Test
+    public void Test(){
+        driver.get("https://demoqa.com");
+    }
+}
--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -1,4 +1,27 @@
 package utilities;
 
-public class TestBase {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public abstract class TestBase {
+    protected static WebDriver driver;
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options= new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver= new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().window().maximize();
+    }
+    @After
+    public void tearDown(){
+        driver.close();
+    }
 }
