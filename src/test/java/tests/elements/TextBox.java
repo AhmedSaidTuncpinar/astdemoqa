cd5a647cca36a9a18a2e00b3cdbce1d75c057fa0
AhmedSaidTuncpinar <ahmtstncpnr@gmail.com>
2023-06-22 13:49:40 +0200

    update

--- src/test/java/tests/elements/TextBox.java
+++ src/test/java/tests/elements/TextBox.java
@@ -1,12 +1,23 @@
 package tests.elements;
 
+import com.github.javafaker.Faker;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import utilities.TestBase;
+
+import java.time.Duration;
 
 public class TextBox extends TestBase {
 
     @Test
     public void Test(){
+        Faker faker= new Faker();
         driver.get("https://demoqa.com");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.findElement(By.xpath("//*[.='Elements']")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.findElement(By.xpath("//*[.='Text Box']")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.findElement(By.xpath("//*[@id='userName']")).sendKeys(faker.name().fullName());
     }
 }
