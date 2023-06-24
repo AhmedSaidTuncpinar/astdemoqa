d3afed017402ebe17c66936705c68d969062ca01
AhmedSaidTuncpinar <ahmtstncpnr@gmail.com>
2023-06-24 11:18:44 +0200

    update

--- src/test/java/tests/elements/CheckBox.java
+++ src/test/java/tests/elements/CheckBox.java
@@ -1,7 +1,19 @@
 package tests.elements;
 
+import com.github.javafaker.Faker;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import utilities.TestBase;
 
-public class CheckBox extends TestBase {
+import java.time.Duration;
 
+public class CheckBox extends TestBase {
+    @Test
+    public void Test(){
+        Faker faker= new Faker();
+        driver.get("https://demoqa.com");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.findElement(By.xpath("//*[.='Elements']")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+    }
 }
