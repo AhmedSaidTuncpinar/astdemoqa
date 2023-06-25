cabadaea03ecabb71742d26a16e86a0f376dde64
AhmedSaidTuncpinar <ahmtstncpnr@gmail.com>
2023-06-25 23:01:02 +0200

    update

--- src/test/java/tests/elements/WebTables.java
+++ src/test/java/tests/elements/WebTables.java
@@ -1,5 +1,6 @@
 package tests.elements;
 
+import com.github.javafaker.Faker;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import utilities.TestBase;
@@ -9,6 +10,7 @@ import java.time.Duration;
 public class WebTables extends TestBase {
     @Test
     public void Test() {
+        Faker faker= new Faker();
         driver.get("https://demoqa.com");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         driver.findElement(By.xpath("//*[.='Elements']")).click();
