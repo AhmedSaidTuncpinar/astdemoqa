fc571dfcfa3f35f660ded3b03b80c377183b39c3
AhmedSaidTuncpinar <ahmtstncpnr@gmail.com>
2023-06-26 08:12:10 +0200

    update

--- src/test/java/tests/elements/WebTables.java
+++ src/test/java/tests/elements/WebTables.java
@@ -26,6 +26,17 @@ public class WebTables extends TestBase {
         WebElement addButton= driver.findElement(By.xpath("//button[@id='addNewRecordButton']"));
         Assert.assertTrue(addButton.isEnabled());
 
+        //Task 2 Add new input by filling the registration page
+        driver.findElement(By.cssSelector("#firstName")).sendKeys(faker.name().firstName());
+        driver.findElement(By.cssSelector("#lastName")).sendKeys(faker.name().lastName());
+        driver.findElement(By.cssSelector("#userEmail")).sendKeys(faker.internet().emailAddress());
+        driver.findElement(By.cssSelector("#age")).sendKeys("33");
+        driver.findElement(By.cssSelector("#salary")).sendKeys("55000");
+        driver.findElement(By.cssSelector("#department")).sendKeys("Project");
+
+
+
+
 
     }
 }
