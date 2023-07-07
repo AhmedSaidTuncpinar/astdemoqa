36fe12993ac8951ff58d3e4e5ad52979120921c3
AhmedSaidTuncpinar <ahmtstncpnr@gmail.com>
2023-07-07 20:42:20 +0200

    update

---
+++ src/test/java/tests/elements/Download.java
@@ -0,0 +1,44 @@
+package tests.elements;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Duration;
+
+public class Download extends TestBase {
+    @Test
+    public void Test() throws InterruptedException{
+        driver.findElement(By.xpath("//*[.='Elements']")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+        driver.findElement(By.cssSelector("#item-7")).click();
+
+        WebElement fileInput= driver.findElement(By.cssSelector("#uploadFile"));
+        Thread.sleep(3000);
+        String filePath1 = "/Users/ahmedsaidtuncpinar/Desktop/downloadFile.jpeg";
+        Path path = Paths.get(filePath1);
+        String absolutePath = path.toAbsolutePath().toString();
+
+        // Enter the file path in the file input element
+        fileInput.sendKeys(absolutePath);
+
+        // Wait for the file to upload (adjust the duration if needed)
+        Thread.sleep(3000);
+
+        // Verify if the file is uploaded successfully
+        WebElement uploadedFile = driver.findElement(By.id("uploadedFilePath"));
+        String uploadedFilePath = uploadedFile.getText();
+
+        String expectedFileName = "downloadFile.jpeg";
+        String actualFileName = uploadedFilePath.substring(uploadedFilePath.lastIndexOf("\\") + 1);
+
+        Assert.assertEquals("File names mismatch", expectedFileName, actualFileName);
+
+
+    }
+}
--- src/test/java/tests/elements/Upload_Download.java
+++ src/test/java/tests/elements/Upload_Download.java
@@ -7,30 +7,33 @@ import org.openqa.selenium.WebElement;
 import utilities.TestBase;
 
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Duration;
 
 public class Upload_Download extends TestBase {
 
     @Test
-    public void Test() throws InterruptedException{
+    public void Test() throws InterruptedException {
         driver.findElement(By.xpath("//*[.='Elements']")).click();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 
         driver.findElement(By.cssSelector("#item-7")).click();
 
-         driver.findElement(By.xpath("//a[text()='Download']")).click();
-        Thread.sleep(3000);
+        driver.findElement(By.xpath("//a[text()='Download']")).click();
 
-         String homeDirectory= System.getProperty("user.dir");
-         String filePath= homeDirectory + "/Users/ahmedsaidtuncpinar/Downloads/sampleFile.jpeg";
+        Thread.sleep(3000); // Wait for the download to complete (adjust the duration if needed)
 
-        boolean isDownloaded= Files.exists(Paths.get(filePath));
+        String filePath = "/Users/ahmedsaidtuncpinar/Downloads/sampleFile.jpeg";
+
+        boolean isDownloaded = Files.exists(Paths.get(filePath));
         System.out.println(isDownloaded);
-       // Assert.assertEquals(isDownloaded, "sampleFile.jpeg");
+
+        Assert.assertTrue("File not downloaded", isDownloaded);
 
 
     }
-
-
 }
+
+
+
