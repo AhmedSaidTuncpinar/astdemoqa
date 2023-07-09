ddb51812a7f464767134262ae9f784170a0f7263
AhmedSaidTuncpinar <ahmtstncpnr@gmail.com>
2023-07-09 08:16:32 +0200

    update

--- src/test/java/tests/homepage/HomePage.java
+++ src/test/java/tests/homepage/HomePage.java
@@ -1,7 +1,24 @@
 package tests.homepage;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import utilities.TestBase;
 
-public class HomePage extends TestBase {
+import java.time.Duration;
 
+public class HomePage extends TestBase {
+    @Test
+    public void Test(){
+       WebElement mainPicture=  driver.findElement(By.cssSelector("a[href='https://demoqa.com']"));
+       mainPicture.click();
+       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        Assert.assertTrue(mainPicture.isSelected());
+
+        WebElement mainBanner= driver.findElement(By.xpath("//img[@class='banner-image']"));
+        mainBanner.click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        Assert.assertTrue(mainBanner.isEnabled());
+    }
 }
